cd72c7d6b38414ee8d587cc112d6368827e1ff7a
V3n3l1n <nqkuv.tam16@gmail.com>
2023-02-20 19:49:31 +0200

    Register design was created

--- VetClinic/View/frmRegister.Designer.cs
+++ VetClinic/View/frmRegister.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             this.lblRegister = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtConfirm = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblRegister
@@ -41,11 +48,83 @@
             this.lblRegister.TabIndex = 1;
             this.lblRegister.Text = "Register";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(98, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(253, 208);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(205, 27);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(98, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(253, 130);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(205, 27);
+            this.txtUsername.TabIndex = 5;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(98, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Confirm Password";
+            // 
+            // txtConfirm
+            // 
+            this.txtConfirm.Location = new System.Drawing.Point(303, 278);
+            this.txtConfirm.Name = "txtConfirm";
+            this.txtConfirm.PasswordChar = '*';
+            this.txtConfirm.Size = new System.Drawing.Size(205, 27);
+            this.txtConfirm.TabIndex = 7;
+            this.txtConfirm.UseSystemPasswordChar = true;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(339, 353);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(154, 51);
+            this.btnRegister.TabIndex = 8;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
             // frmRegister
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtConfirm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblRegister);
             this.Name = "frmRegister";
             this.Text = "Register";
@@ -57,5 +136,12 @@
         #endregion
 
         private Label lblRegister;
+        private Label label1;
+        private TextBox txtPassword;
+        private Label label2;
+        private TextBox txtUsername;
+        private Label label3;
+        private TextBox txtConfirm;
+        private Button btnRegister;
     }
 }
--- VetClinic/View/frmRegister.cs
+++ VetClinic/View/frmRegister.cs
@@ -16,5 +16,10 @@ namespace VetClinic.View
         {
             InitializeComponent();
         }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }

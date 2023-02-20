caf53524e7af82fc9258084b45caac6cb17b63f9
Evgeni32 <evgeni.gena32@abv.bg>
2023-02-20 11:49:59 +0200

    update table

--- VetClinic/Controller/dbo.Table.sql
+++ VetClinic/Controller/dbo.Table.sql
@@ -1,6 +1,6 @@
-﻿CREATE TABLE [dbo].[Table]
+﻿CREATE TABLE [dbo].[User]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Username] NCHAR(50) NOT NULL, 
-    [Password] NVARCHAR(50) NOT NULL
+    [Password] NVARCHAR(20) NOT NULL
 )

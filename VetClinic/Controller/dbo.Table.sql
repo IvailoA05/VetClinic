320ed5db9be2d0040bb3d96749a7afa913c1eb5d
IvailoA05 <ivailooo55@abv.bg>
2023-02-20 18:05:26 +0200

    Edited database

--- VetClinic/Controller/dbo.Table.sql
+++ VetClinic/Controller/dbo.Table.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[User]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Username] NCHAR(50) NOT NULL, 
     [Password] NVARCHAR(20) NOT NULL
 )

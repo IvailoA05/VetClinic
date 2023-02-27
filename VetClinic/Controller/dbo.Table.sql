48cdd443f834060680aac7e6fe40d5fcb29a9bc4
Ivailo Arabadjiev <83120979+IvailoA05@users.noreply.github.com>
2023-02-27 11:51:47 +0200

    Update dbo.Table.sql

--- VetClinic/Controller/dbo.Table.sql
+++ VetClinic/Controller/dbo.Table.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[User]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Username] NCHAR(50) NOT NULL, 
+    [Username] NCHAR(100) NOT NULL, 
     [Password] NVARCHAR(20) NOT NULL
 )
+
